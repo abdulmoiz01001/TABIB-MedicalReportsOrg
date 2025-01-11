12ce72585ff72c6eed18f08685ee0496c46e26f8
abdulmoiz01001 <profession.ai01001@hotmail.com>
2025-01-11 23:46:47 +0500

    changes in Dashboard

--- src/components/DashboardCompnents/DashboardStaticsComp.jsx
+++ src/components/DashboardCompnents/DashboardStaticsComp.jsx
@@ -13,32 +13,32 @@ import TABIATScoreComp from './TABIATScoreComp'
 const DashboardStaticsComp = () => {
   return (
     <>
-    <div className='w-full flex flex-row justify-start items-start mx-4 h-full border-2 border-blue-900' >
-      <div className='w-[75%] flex flex-col h-full border-2 border-red-900 ' >
-         <div className='w-full flex px-1 justify-evenly items-center h-[20%] border-2 border-red-900' >
+    <div className='w-full flex flex-row justify-start items-start mx-4 h-full  border-blue-900' >
+      <div className='w-[75%] flex flex-col h-full  border-red-900 ' >
+         <div className='w-full flex px-1 justify-evenly items-center h-[20%]  border-red-900' >
            <ReportBoxComp title={"Hypertensive"} count={"2091"} female={"1123"} male={"800"} /> 
            <ReportBoxComp title={"Total Test Reports"} count={"3942"}  /> 
            <PulseRateAndBloodPresureComp  />
            <ReportBoxComp title={"Test Report Year 2025"} count={"7042"}  /> 
            <ReportBoxComp title={"Test Report Month Jan"} count={"403"}  /> 
          </div>
-         <div className='w-full flex px-1 flex-row justify-start items-start h-[80%] border-2 border-yellow-300 ' >
-           <div className='w-[26%] flex flex-col justify-between items-end h-full border-2 border-red-900' >
+         <div className='w-full flex px-1 flex-row justify-start items-start h-[80%]  border-yellow-300 ' >
+           <div className='w-[26%] flex flex-col justify-between items-end h-full  border-red-900' >
             <HypertensionComp />
             <OtherTemperamentIndicesComp />
 
            </div>
-           <div className='w-[36.5%] px-1 flex flex-col justify-start items-start h-full border-2 border-red-900' >
+           <div className='w-[36.5%] px-1 gap-1 flex flex-col justify-start items-start h-full  border-red-900' >
             <NoOfPatientsVsAgeComp />
             <TrendOfHypertensionComp />
             <TemperamentChartComp />
            </div>
-           <div className='w-[37.5%] flex flex-col justify-center items-start h-full border-2 border-red-900' >
+           <div className='w-[37.5%] flex flex-col justify-center items-start h-full  border-red-900' >
 
            </div>
          </div>
       </div>
-      <div className='w-[25%] h-full border-2 flex flex-col gap-1 border-green-900' >
+      <div className='w-[25%] h-full  flex flex-col gap-1 border-green-900' >
           <TemperamentCommunityComp />
           <DominantBodyCommunityComp />
           <TABIATScoreComp />
--- src/components/DashboardCompnents/HeadingComp.jsx
+++ src/components/DashboardCompnents/HeadingComp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeadingComp = () => {
   return (
     <>
-    <div className='w-full flex justify-between items-center border-2 px-4  border-red-900 ' >
+    <div className='w-full flex justify-between items-center  px-4  border-red-900 ' >
      <img src="logo.svg" alt="logo" className='w-[81px] h-[50px] ' />
      <h1 className='text-2xl text-[#CC0001] font-bold'>TABIAT: Temperament and Blood-Pressure Indication and Tracking (powered by TABIB)</h1>
      <img src="logo.svg" alt="logo" className='w-[81px] h-[50px] ' />
